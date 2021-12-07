39f319573312da70832b06ac0a8f74ba885fdd72
Rishabh Jain <rjain57@uwo.ca>
2021-12-06 21:51:44 -0500

    TCP to UDP

--- client.py
+++ client.py
@@ -5,11 +5,19 @@ import sys
 import sys
 import argparse
 from urllib.parse import urlparse
+import struct
+import hashlib
 import selectors
+
+UDP_IP = "localhost"
 
 # Define a constant for our buffer size
 
 BUFFER_SIZE = 1024
+
+# Define a maximum string size for the text we'll be sending along.
+
+MAX_STRING_SIZE = 256
 
 # Selector for helping us select incoming data from the server and messages typed in by the user.
 
@@ -23,13 +31,18 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 user = ''
 
+host = ""
+port = 0
+
+
 # Signal handler for graceful exiting.  Let the server know when we're gone.
 
 def signal_handler(sig, frame):
     print('Interrupt received, shutting down ...')
-    message=f'DISCONNECT {user} CHAT/1.0\n'
-    client_socket.send(message.encode())
+    message = f'DISCONNECT {user} CHAT/1.0\n'
+    sendMessage(client_socket, message)
     sys.exit(0)
+
 
 # Simple function for setting up a prompt for the user.
 
@@ -38,9 +51,10 @@ def do_prompt(skip_line=False):
         print("")
     print("> ", end='', flush=True)
 
+
 # Read a single line (ending with \n) from a socket and return it.
 # We will strip out the \r and the \n in the process.
-
+"""
 def get_line_from_socket(sock):
 
     done = False
@@ -54,12 +68,14 @@ def get_line_from_socket(sock):
         else:
             line = line + char
     return line
+"""
+
 
 # Function to handle incoming messages from server.  Also look for disconnect messages to shutdown and messages for sending and receiving files.
 
 def handle_message_from_server(sock, mask):
-    message=get_line_from_socket(sock)
-    words=message.split(' ')
+    message = get_line_from_socket(sock)
+    words = message.split(' ')
     print()
 
     # Handle server disconnection.
@@ -76,6 +92,7 @@ def handle_message_from_server(sock, mask):
         if (os.path.exists(filename)):
             filesize = os.path.getsize(filename)
             header = f'Content-Length: {filesize}\n'
+            sendMessage(sock, message)
             sock.send(header.encode())
             with open(filename, 'rb') as file_to_send:
                 while True:
@@ -87,17 +104,17 @@ def handle_message_from_server(sock, mask):
         else:
             header = f'Content-Length: -1\n'
             sock.send(header.encode())
-        sock.setblocking(False)
-            
+        # sock.setblocking(False)
+
     # Handle file attachment request.
 
     elif words[0] == 'ATTACHMENT':
         filename = words[1]
         sock.setblocking(True)
         print(f'Incoming file: {filename}')
-        origin=get_line_from_socket(sock)
+        origin = get_line_from_socket(sock)
         print(origin)
-        contentlength=get_line_from_socket(sock)
+        contentlength = get_line_from_socket(sock)
         print(contentlength)
         length_words = contentlength.split(' ')
         if (len(length_words) != 2) or (length_words[0] != 'Content-Length:'):
@@ -110,7 +127,7 @@ def handle_message_from_server(sock, mask):
                     chunk = sock.recv(BUFFER_SIZE)
                     bytes_read += len(chunk)
                     file_to_write.write(chunk)
-        sock.setblocking(False)
+        # sock.setblocking(False)
         do_prompt()
 
     # Handle regular messages.
@@ -119,21 +136,72 @@ def handle_message_from_server(sock, mask):
         print(message)
         do_prompt()
 
+
 # Function to handle incoming messages from server.
 
 def handle_keyboard_input(file, mask):
-    line=sys.stdin.readline()
+    line = sys.stdin.readline()
     message = f'@{user}: {line}'
-    client_socket.send(message.encode())
+    sendMessage(client_socket, message)
     do_prompt()
+
+
+def sendMessage(sock, message):
+    sequence_number = 0
+    data = message.encode()
+    size = len(data)
+
+    packet_tuple = (sequence_number, size, data)
+    packet_structure = struct.Struct(f'I I {MAX_STRING_SIZE}s')
+    packed_data = packet_structure.pack(*packet_tuple)
+    checksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+    packet_tuple = (sequence_number, size, data, checksum)
+    UDP_packet_structure = struct.Struct(f'I I {MAX_STRING_SIZE}s 32s')
+    UDP_packet = UDP_packet_structure.pack(*packet_tuple)
+    print("Message to send: ", data)
+    print("host, port", host, port)
+    sock.sendto(UDP_packet, (host, port))
+
+
+# Function that reads line from socket
+
+def get_line_from_socket(sock):
+    received_packet, addr = sock.recvfrom(1024)
+    unpacker = struct.Struct(f'I I {MAX_STRING_SIZE}s 32s')
+    UDP_packet = unpacker.unpack(received_packet)
+
+    # Extract out data that was received from the packet.  It unpacks to a tuple, but it's easy enough to split apart.
+    received_sequence = UDP_packet[0]
+    received_size = UDP_packet[1]
+    received_data = UDP_packet[2]
+    received_checksum = UDP_packet[3]
+
+    # Print out what we received.
+    print("Packet received from:", addr)
+    print("Packet data:", UDP_packet)
+
+    values = (received_sequence, received_size, received_data)
+    packer = struct.Struct(f'I I {MAX_STRING_SIZE}s')
+    packed_data = packer.pack(*values)
+    computed_checksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+    if received_checksum == computed_checksum:
+        print('Received and computed checksums match, so packet can be processed')
+        received_text = received_data[:received_size].decode()
+        print(f'Message text was:  {received_text}')
+        return received_text
+    else:
+        print('Received and computed checksums do not match, so packet is corrupt and discarded')
+
 
 # Our main function.
 
 def main():
-
     global user
     global client_socket
-
+    global host
+    global port
     # Register our signal handler for shutting down.
 
     signal.signal(signal.SIGINT, signal_handler)
@@ -165,24 +233,25 @@ def main():
 
     print('Connecting to server ...')
     try:
-        client_socket.connect((host, port))
+        # client_socket.connect((host, port))
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     except ConnectionRefusedError:
         print('Error:  That host or port is not accepting connections.')
         sys.exit(1)
 
     # The connection was successful, so we can prep and send a registration message.
-    
+
     print('Connection to server established. Sending intro message...\n')
     message = f'REGISTER {user} CHAT/1.0\n'
-    client_socket.send(message.encode())
-   
+    sendMessage(client_socket, message)
+    '''
     # If we have terms to follow, we send them now.  Otherwise, we send an empty line to indicate we're done with registration.
 
     if follow != []:
         message = f'Follow: {follow[0]}\n\n'
     else:
         message = '\n'
-    client_socket.send(message.encode())
+    sendMessage(client_socket, message)
    
     # Receive the response from the server and start taking a look at it
 
@@ -201,23 +270,21 @@ def main():
         print('Registration successful.  Ready for messaging!')
 
     # Set up our selector.
-
-    client_socket.setblocking(False)
+    '''
+    # client_socket.setblocking(False)
     sel.register(client_socket, selectors.EVENT_READ, handle_message_from_server)
     sel.register(sys.stdin, selectors.EVENT_READ, handle_keyboard_input)
-    
-    # Prompt the user before beginning.
 
+    # Prompt the user before beginning.
     do_prompt()
 
     # Now do the selection.
 
-    while(True):
+    while (True):
         events = sel.select()
         for key, mask in events:
             callback = key.data
-            callback(key.fileobj, mask)    
-
+            callback(key.fileobj, mask)
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -4,11 +4,20 @@ import datetime
 import signal
 import sys
 import selectors
+import struct
+import hashlib
 from string import punctuation
+
+UDP_IP = "localhost"
+UDP_PORT = 54321
 
 # Constant for our buffer size
 
 BUFFER_SIZE = 1024
+
+# Define a maximum string size for the text we'll be receiving.
+
+MAX_STRING_SIZE = 256
 
 # Selector for helping us select incoming data and connections from multiple sources.
 
@@ -18,31 +27,51 @@ sel = selectors.DefaultSelector()
 
 client_list = []
 
+host = ""
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
 # Signal handler for graceful exiting.  We let clients know in the process so they can disconnect too.
 
 def signal_handler(sig, frame):
     print('Interrupt received, shutting down ...')
-    message='DISCONNECT CHAT/1.0\n'
+    message = 'DISCONNECT CHAT/1.0\n'
     for reg in client_list:
-        reg[1].send(message.encode())
+        send_msg(sock, reg[1], message)
     sys.exit(0)
 
-# Read a single line (ending with \n) from a socket and return it.
-# We will strip out the \r and the \n in the process.
+
+# Function that reads line from socket
 
 def get_line_from_socket(sock):
+    received_packet, addr = sock.recvfrom(1024)
+    unpacker = struct.Struct(f'I I {MAX_STRING_SIZE}s 32s')
+    UDP_packet = unpacker.unpack(received_packet)
 
-    done = False
-    line = ''
-    while (not done):
-        char = sock.recv(1).decode()
-        if (char == '\r'):
-            pass
-        elif (char == '\n'):
-            done = True
-        else:
-            line = line + char
-    return line
+    # Extract out data that was received from the packet.  It unpacks to a tuple, but it's easy enough to split apart.
+    received_sequence = UDP_packet[0]
+    received_size = UDP_packet[1]
+    received_data = UDP_packet[2]
+    received_checksum = UDP_packet[3]
+
+    # Print out what we received.
+    print("Packet received from:", addr)
+    print("Packet data:", UDP_packet)
+
+    values = (received_sequence, received_size, received_data)
+    packer = struct.Struct(f'I I {MAX_STRING_SIZE}s')
+    packed_data = packer.pack(*values)
+    computed_checksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+    if received_checksum == computed_checksum:
+        print('Received and computed checksums match, so packet can be processed')
+        received_text = received_data[:received_size].decode()
+        print(f'Message text was:  {received_text}')
+        return received_text, addr[1]
+    else:
+        print('Received and computed checksums do not match, so packet is corrupt and discarded')
+
 
 # Search the client list for a particular user.
 
@@ -52,19 +81,24 @@ def client_search(user):
             return reg[1]
     return None
 
-# Search the client list for a particular user by their socket.
 
-def client_search_by_socket(sock):
+# Search the client list for a particular user by their port.
+
+def client_search_by_port(port):
+    print("client list: ", client_list)
     for reg in client_list:
-        if reg[1] == sock:
+        print(reg)
+        if reg[1] == port:
             return reg[0]
-    return None
+    return False
+
 
 # Add a user to the client list.
 
 def client_add(user, conn, follow_terms):
     registration = (user, conn, follow_terms)
     client_list.append(registration)
+
 
 # Remove a client when disconnected.
 
@@ -73,6 +107,7 @@ def client_remove(user):
         if reg[0] == user:
             client_list.remove(reg)
             break
+
 
 # Function to list clients.
 
@@ -86,6 +121,7 @@ def list_clients():
         else:
             list = f'{list}, {reg[0]}'
     return list
+
 
 # Function to return list of followed topics of a user.
 
@@ -103,6 +139,7 @@ def client_follows(user):
             return list
     return None
 
+
 # Function to add to list of followed topics of a user, returning True if added or False if topic already there.
 
 def client_add_follow(user, topic):
@@ -114,6 +151,7 @@ def client_add_follow(user, topic):
                 reg[2].append(topic)
                 return True
     return None
+
 
 # Function to remove from list of followed topics of a user, returning True if removed or False if topic was not already there.
 
@@ -127,49 +165,59 @@ def client_remove_follow(user, topic):
                 return False
     return None
 
+
 # Function to read messages from clients.
 
-def read_message(sock, mask):
-    message = get_line_from_socket(sock)
+def read_message(message, addr):
+    print("message reading")
+    # message, addr = get_line_from_socket(sock)
 
     # Does this indicate a closed connection?
 
     if message == '':
         print('Closing connection')
-        sel.unregister(sock)
-        sock.close()
+        # sel.unregister(sock)
+        # sock.close()
 
     # Receive the message.  
 
     else:
-        user = client_search_by_socket(sock)
+        user = client_search_by_port(addr)
         print(f'Received message from user {user}:  ' + message)
-        words = message.split(' ')
+        split_words = message.split(' ')
+        words = []
+        # Remove trailing
+        for i in split_words:
+            words.append(i.strip("\n\r"))
+        # Check for client disconnections.
 
-        # Check for client disconnections.  
- 
         if words[0] == 'DISCONNECT':
             print('Disconnecting user ' + user)
             client_remove(user)
-            sel.unregister(sock)
-            sock.close()
+            # sel.unregister(sock)
+            # sock.close()
 
         # Check for specific commands.
 
         elif ((len(words) == 2) and ((words[1] == '!list') or (words[1] == '!exit') or (words[1] == '!follow?'))):
             if words[1] == '!list':
+                print('!list')
                 response = list_clients() + '\n'
-                sock.send(response.encode())
+                send_msg(sock, addr, response)
+                # sock.send(response.encode())
             elif words[1] == '!exit':
                 print('Disconnecting user ' + user)
-                response='DISCONNECT CHAT/1.0\n'
-                sock.send(response.encode())
+                response = 'DISCONNECT CHAT/1.0\n'
+                send_msg(sock, addr, response)
+                # sock.send(response.encode())
                 client_remove(user)
-                sel.unregister(sock)
-                sock.close()
+                # sel.unregister(sock)
+                # sock.close()
             elif words[1] == '!follow?':
+                print('!follow?')
                 response = client_follows(user) + '\n'
-                sock.send(response.encode())
+                send_msg(sock, addr, response)
+                # sock.send(response.encode())
 
         # Check for specific commands with a parameter.
 
@@ -180,30 +228,33 @@ def read_message(sock, mask):
                     response = f'Now following {topic}\n'
                 else:
                     response = f'Error:  Was already following {topic}\n'
-                sock.send(response.encode())
+                # sock.send(response.encode())
+                send_msg(sock, addr, response)
             elif words[1] == '!unfollow':
                 topic = words[2]
                 if topic == '@all':
                     response = 'Error:  All users must follow @all\n'
-                elif topic == '@'+user:
+                elif topic == '@' + user:
                     response = 'Error:  Cannot unfollow yourself\n'
                 elif client_remove_follow(user, topic):
                     response = f'No longer following {topic}\n'
                 else:
                     response = f'Error:  Was not following {topic}\n'
-                sock.send(response.encode())
+                # sock.send(response.encode())
+                send_msg(sock, addr, response)
 
         # Check for user trying to upload/attach a file.  We strip the message to keep the user and any other text to help forward the file.  Will
         # send it to interested users like regular messages.
 
         elif ((len(words) >= 3) and (words[1] == '!attach')):
-            sock.setblocking(True)
+            # sock.setblocking(True)
             filename = words[2]
             words.remove('!attach')
             words.remove(filename)
             response = f'ATTACH {filename} CHAT/1.0\n'
-            sock.send(response.encode())
-            header = get_line_from_socket(sock)
+            # sock.send(response.encode())
+            send_msg(sock, addr, response)
+            header, addr = get_line_from_socket(sock)
             header_words = header.split(' ')
             if (len(header_words) != 2) or (header_words[0] != 'Content-Length:'):
                 response = f'Error:  Invalid attachment header\n'
@@ -221,7 +272,8 @@ def read_message(sock, mask):
                         for word in words:
                             if ((term == word.rstrip(punctuation)) and not forwarded):
                                 interested_clients.append(reg[1])
-                                reg[1].send(attach_notice.encode())
+                                send_msg(reg[1], addr, attach_notice)
+                                # reg[1].send(attach_notice.encode())
                                 forwarded = True
                 bytes_read = 0
                 bytes_to_read = int(attach_size)
@@ -231,8 +283,9 @@ def read_message(sock, mask):
                     for client in interested_clients:
                         client.send(chunk)
                 response = f'Attachment {filename} attached and distributed\n'
-            sock.send(response.encode())
-            sock.setblocking(False)
+            # sock.send(response.encode())
+            send_msg(sock, addr, response)
+            # sock.setblocking(False)
 
         # Look for follow terms and dispatch message to interested users.  Send at most only once, and don't send to yourself.  Trailing punctuation is stripped.
         # Need to re-add stripped newlines here.
@@ -245,96 +298,129 @@ def read_message(sock, mask):
                 for term in reg[2]:
                     for word in words:
                         if ((term == word.rstrip(punctuation)) and not forwarded):
-                            client_sock = reg[1]
+                            client_port = reg[1]
                             forwarded_message = f'{message}\n'
-                            client_sock.send(forwarded_message.encode())
+                            # client_port.send(forwarded_message.encode())
+                            send_msg(sock, client_port, forwarded_message)
                             forwarded = True
-        
+
+
+def send_msg(sock, port, message):
+    print("test")
+    sequence_number = 0
+    data = message.encode()
+    size = len(data)
+
+    packet_tuple = (sequence_number, size, data)
+    packet_structure = struct.Struct(f'I I {MAX_STRING_SIZE}s')
+    packed_data = packet_structure.pack(*packet_tuple)
+    checksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+    packet_tuple = (sequence_number, size, data, checksum)
+    UDP_packet_structure = struct.Struct(f'I I {MAX_STRING_SIZE}s 32s')
+    UDP_packet = UDP_packet_structure.pack(*packet_tuple)
+    print("SendMessage: ", data)
+    print("host, port", host, port)
+    sock.sendto(UDP_packet, (host, port))
+
+
+# def check_new_user(message_parts):
+
 
 # Function to accept and set up clients.
 
 def accept_client(sock, mask):
-    conn, addr = sock.accept()
-    print('Accepted connection from client address:', addr)
-    message = get_line_from_socket(conn)
+    # conn, addr = sock.accept()
+    conn = sock.getsockname()[0]
+    addr = sock.getsockname()[1]
+    message, addr = get_line_from_socket(sock)
     message_parts = message.split()
+    # print(message)
+    # message = "test sending from server"
+    # send_msg(sock, addr, message)
 
     # Check format of request.
-
-    if ((len(message_parts) != 3) or (message_parts[0] != 'REGISTER') or (message_parts[2] != 'CHAT/1.0')):
-        print('Error:  Invalid registration message.')
-        print('Received: ' + message)
-        print('Connection closing ...')
-        response='400 Invalid registration\n'
-        conn.send(response.encode())
-        conn.close()
-
-    # If request is properly formatted and user not already listed, go ahead with registration.
-
-    else:
-        user = message_parts[1]
-        if user == 'all':
-            print('Error:  Client cannot use reserved user name \'all\'.')
+    # if check_new_user(message_parts)
+    if not client_search_by_port(addr):
+        print('Accepted connection from client address:', addr)
+        if ((len(message_parts) != 3) or (message_parts[0] != 'REGISTER') or (message_parts[2] != 'CHAT/1.0')):
+            print('Error:  Invalid registration message.')
+            print('Received: ' + message)
             print('Connection closing ...')
-            response='402 Forbidden user name\n'
-            conn.send(response.encode())
-            conn.close()
+            response = '400 Invalid registration\n'
+            send_msg(sock, addr, response)
+            # conn.send(response.encode())
+            # conn.close()
 
-        elif (client_search(user) == None):
+        # If request is properly formatted and user not already listed, go ahead with registration.
 
-            # Check for following terms or an issue with the request.
+        else:
+            user = message_parts[1]
+            if user == 'all':
+                print('Error:  Client cannot use reserved user name \'all\'.')
+                print('Connection closing ...')
+                response = '402 Forbidden user name\n'
+                send_msg(sock, addr, response)
+                # conn.send(response.encode())
+                # conn.close()
 
-            follow_terms = []
-            follow_message = get_line_from_socket(conn)
-            if follow_message != "":
-                if follow_message.startswith('Follow: '):
-                    follow_terms = follow_message[len('Follow: '):].split(',')
-                    blank_line = get_line_from_socket(conn)
-                    if blank_line != "":
+            elif (client_search(user) == None):
+                follow_terms = []
+                """
+                # Check for following terms or an issue with the request.
+                follow_message = get_line_from_socket(sock)
+                if follow_message != "":
+                    if follow_message.startswith('Follow: '):
+                        follow_terms = follow_message[len('Follow: '):].split(',')
+                        blank_line = get_line_from_socket(sock)
+                        if blank_line != "":
+                            print('Error:  Invalid registration message.  Issue in follow list.')
+                            print('Received: ' + message)
+                            print('Connection closing ...')
+                            response = '400 Invalid registration\n'
+                            conn.send(response.encode())
+                            conn.close()
+                            return
+                    else:
                         print('Error:  Invalid registration message.  Issue in follow list.')
                         print('Received: ' + message)
                         print('Connection closing ...')
-                        response='400 Invalid registration\n'
+                        response = '400 Invalid registration\n'
                         conn.send(response.encode())
                         conn.close()
                         return
-                else:
-                    print('Error:  Invalid registration message.  Issue in follow list.')
-                    print('Received: ' + message)
-                    print('Connection closing ...')
-                    response='400 Invalid registration\n'
-                    conn.send(response.encode())
-                    conn.close()
-                    return
+                """
+                # Add the user to their follow list, so @user finds them.  We'll also do @all as well for broadcast messages.
 
-            # Add the user to their follow list, so @user finds them.  We'll also do @all as well for broadcast messages.
+                follow_terms.append(f'@{user}')
+                follow_terms.append('@all')
 
-            follow_terms.append(f'@{user}')
-            follow_terms.append('@all')
+                # Finally add the user.
 
-            # Finally add the user.
+                client_add(user, addr, follow_terms)
+                print(f'Connection to client established, waiting to receive messages from user \'{user}\'...')
+                response = '200 Registration succesful\n'
+                send_msg(sock, addr, response)
+                # conn.send(response.encode())
+                # conn.setblocking(False)
+                # sel.register(addr, selectors.EVENT_READ, read_message)
 
-            client_add(user,conn, follow_terms)
-            print(f'Connection to client established, waiting to receive messages from user \'{user}\'...')
-            response='200 Registration succesful\n'
-            conn.send(response.encode())
-            conn.setblocking(False)
-            sel.register(conn, selectors.EVENT_READ, read_message)
+            # If user already in list, return a registration error.
 
-        # If user already in list, return a registration error.
-
-        else:
-            print('Error:  Client already registered.')
-            print('Connection closing ...')
-            response='401 Client already registered\n'
-            conn.send(response.encode())
-            conn.close()
+            else:
+                print('Error:  Client already registered.')
+                print('Connection closing ...')
+                response = '401 Client already registered\n'
+                send_msg(sock, addr, response)
+                # conn.send(response.encode())
+                # conn.close()
+    else:
+        read_message(message, addr)
 
 
 # Our main function.
 
 def main():
-
     # Register our signal handler for shutting down.
 
     signal.signal(signal.SIGINT, signal_handler)
@@ -342,22 +428,21 @@ def main():
     # Create the socket.  We will ask this to work on any interface and to pick
     # a free port at random.  We'll print this out for clients to use.
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('', 0))
-    print('Will wait for client connections at port ' + str(server_socket.getsockname()[1]))
-    server_socket.listen(100)
-    server_socket.setblocking(False)
-    sel.register(server_socket, selectors.EVENT_READ, accept_client)
+    sock.bind(('', 0))
+    print('Will wait for client connections at port ' + str(sock.getsockname()[1]))
+    # sock.listen(100)
+    # sock.setblocking(False)
+    sel.register(sock, selectors.EVENT_READ, accept_client)
     print('Waiting for incoming client connections ...')
-     
+
     # Keep the server running forever, waiting for connections or messages.
-    
-    while(True):
+
+    while (True):
         events = sel.select()
         for key, mask in events:
             callback = key.data
-            callback(key.fileobj, mask)    
+            callback(key.fileobj, mask)
+
 
 if __name__ == '__main__':
     main()
-
